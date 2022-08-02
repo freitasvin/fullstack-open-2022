26f2f6114220b4e58ba9af40e5db4510717873fe
Vinícius <viniciusalmeidafreitas@gmail.com>
2022-08-02 11:21:45 -0300

    2.1 course information step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,70 @@
+const Header = ({name}) => {
+  return(
+    <h1>{name}</h1>
+  )
+}
+
+const Part = ({part, exercises}) => {
+  return(
+    <p>{part} {exercises}</p>
+  )
+}
+
+const Content = ({course}) => {
+  return(
+    <>
+      {course.parts.map((part) => { 
+        return(
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+        )
+      })}
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
