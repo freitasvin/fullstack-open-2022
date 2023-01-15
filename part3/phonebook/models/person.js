88b5029763398e96f275377c99ac3b12a2765642
Freitas <viniciusalmeidafreitas@gmail.com>
2023-01-15 20:47:33 -0300

    3.20 Phonebook database - step8

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -18,7 +18,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true, 
   },
-  number: String
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}\-?\d{6,10}$/.test(v)
+      },
+      message: '{VALUE} is not a valid phone number!'
+    }
+  }
 })
 
 personSchema.set('toJSON', {
