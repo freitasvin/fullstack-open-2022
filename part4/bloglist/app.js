ba569a95d15032947f0d8a4401b7ee65a54f0ce0
Freitas <viniciusalmeidafreitas@gmail.com>
2023-01-17 20:01:19 -0300

    4.2 Blog list - step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('Connected to mongodb')
+  })
+  .catch((error) => {
+    logger.error('error connecting to mongodv: ', error.message)
+  })
+
+app.use(express.json())
+app.use(cors())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,28 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blogs')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+module.exports = blogsRouter
