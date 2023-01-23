cb27a83d143606226f53a615461872082970cdf4
Freitas <viniciusalmeidafreitas@gmail.com>
2023-01-22 23:07:09 -0300

    4.9 Blog list tests - step2

--- part4/bloglist/test/blog_api.test.js
+++ part4/bloglist/test/blog_api.test.js
@@ -29,6 +29,11 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(2)
 })
 
+test('the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
 afterAll(async () => {
   await mongoose.connection.close()
 })
