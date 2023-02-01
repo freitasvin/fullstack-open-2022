f9d13d4f325f446840ce2fb04acfb0bb21316ffd
Freitas <viniciusalmeidafreitas@gmail.com>
2023-02-01 16:18:07 -0300

    5.7 Blog list frontend - step6,7

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -97,7 +97,7 @@ const App = () => {
       </div>
       <div>
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog}/>
         )}  
       </div>
     </div>
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,5 +1,28 @@
-export const Blog = ({blog}) => (
-  <div className='blog'>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+export const Blog = ({blog}) => {
+  const [buttonText, setButtonText] = useState('view')
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const handleClickDetails = () => {
+    setViewDetails(!viewDetails)
+    setButtonText(viewDetails === true ? 'view' : 'hide')
+  }
+
+  return (
+    <div className='blog'>
+      {blog.title} {blog.author}
+      <button onClick={handleClickDetails}>{buttonText}</button>
+      {viewDetails && 
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes}
+            <button onClick={() => {}}>like</button>
+          </div>
+          <div>{blog.user.username}</div>
+        </div>
+      }
+    </div>  
+  )
+}
