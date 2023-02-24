dd13ee16b6d7aec275a32357a9f98ac8edcdda86
freitasvin <viniciusalmeidafreitas@gmail.com>
2023-02-24 00:18:55 -0300

    pollyfill

--- part7/webpack-part7/src/index.js
+++ part7/webpack-part7/src/index.js
@@ -1,8 +1,13 @@
+import PromisePolyfill from 'promise-polyfill'
 import 'core-js/stable/index.js'
 import 'regenerator-runtime/runtime.js'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
